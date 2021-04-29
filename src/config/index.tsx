5dbf42d762f5e8c0f524d55642d316b64feceaaf
苏柏良 <1978535811@qq.com>
2021-04-29 22:00:35 +0800

    修改为线下开发

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -4,9 +4,9 @@
 // export const perfix = '/bghouse';
 
 // 线下开发
-export const baseURL: string = 'http://localhost:8080/bghouse'; // 基础URL
+export const baseURL: string = 'http://localhost:8080'; // 基础URL
 export const websocketBaseURL: string = 'ws://localhost:8090/ws'; // 聊天服务器
-export const perfix = '/bghouse';
+export const perfix = '';
 
 export const timeout: number = 1000 * 600; // 请求超时事件
 export const defaultPageSize: number = 10; // 房间列表的每页显示的个数
